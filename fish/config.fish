52d915ee58f9198ad3c3b1fc77356077045783f7
Balaji Damodaran <damodaran.balaji@gmail.com>
2025-10-25 15:58:35 +0200

    fish

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# starship init fish | source
+
+alias g='git'
+alias gpl='git pull'
+alias gpu='git push'
+alias gst='git status'
+alias ga='git add'
+alias gsh='git stash'
+alias gco='git commit'
+alias gr='git rm'
+alias gfu='git fetch upstream'
+alias vim=nvim
+alias ls='ls --color=auto'
+alias tmux='tmux new -A -s main'
+alias e='emacsclient -nw -c -a ""'
+
+export VISUAL="emacsclient -c -nw -a ''"
+export EDITOR="emacsclient -c -nw -a ''"
